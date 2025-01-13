67280766a40be9088a24d04339d4cb0b61eb24e5
LookAtFr3sn0 <lookatfr3sn0@gmail.com>
2025-01-13 17:49:06 +0100

    Implement geolocation feature

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,56 +1,65 @@
+<script setup>
+import { ref } from 'vue';
+import { IonContent, IonPage, IonSpinner } from '@ionic/vue';
+import { Geolocation } from '@capacitor/geolocation';
+
+const weatherIcon = ref('Sunny');
+const city = ref('Rome');
+const currentLocation = ref({latitude: 41.9, longitude: 12.5});
+const loading = ref(true);
+
+const getLocation = async () => {
+  let coords = (await Geolocation.getCurrentPosition()).coords;
+  currentLocation.value = {
+    latitude: coords.latitude,
+    longitude: coords.longitude
+  };
+  loading.value = false;
+};
+try {
+  getLocation();
+} catch (error) {
+  console.error('Error getting location', error);
+}
+</script>
+
 <template>
   <ion-page>
-    <ion-header :translucent="true">
-      <ion-toolbar>
-        <ion-title>Blank</ion-title>
-      </ion-toolbar>
-    </ion-header>
-
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <div id="weather" v-if="loading">
+        <div id="location">
+          <ion-spinner name="crescent" />
+          <span>Loading</span>
+        </div>
+      </div>
+      <div id="weather" v-else>
+        <div id="location">
+          <span class="material-symbols-outlined">{{ weatherIcon }}</span>
+          <span>{{ city }}</span>
+        </div>
+        <span id="coords">{{ `${currentLocation.latitude}, ${currentLocation.longitude}` }}</span>
       </div>
     </ion-content>
   </ion-page>
 </template>
 
-<script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-</script>
-
 <style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
+#weather {
+  position: relative;
+  top: 25%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 0.2rem;
+  display: flex;
+  flex-direction: column;
 }
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
+#location {
+  display: flex;
+  gap: 0.5rem;
+  font-size: 1.5rem;
 }
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
+#coords {
+  font-size: 0.75rem;
 }
 </style>
